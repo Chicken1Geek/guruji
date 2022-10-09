014c58358a1ad5fe9153327c7f721e684820a5d3
Sachin Sankar <sachinpannadi@gmail.com>
2022-10-09 08:39:04 +0530

    Add files via upload

--- globals.py
+++ globals.py
@@ -1,4 +1,9 @@
-Templates = [{'id':'phy0001','name':'Applications of electromagnetic principles in modern weapon systems.','author':'Sachin Sankar','pageCount':18,'tag':'Physics'},
-{'id':'phy0002','name':'Finding refractive index of water and oil using a eqiconvex lens and a adjustable object needle.','author':'Dhaknesh Raj','pageCount':11,'tag':'Physics'}]
+Templates = [{
+    'id':'phy0001','name':'Applications of electromagnetic principles in modern weapon systems','author':'Sachin Sankar','pageCount':18,'tag':'Physics'
+},
+    {
+        'id':'phy0002','name':'Finding refractive index of water and oil using a eqiconvex lens and a adjustable object needle.','author':'Dhaknesh Raj','pageCount':11,'tag':'Physics'
+    },
+    {'id':'phy0003','name':'Study the viscosity of different engine oils','author':'Sachin Sankar','pageCount':11,'tag':'Physics'}]
 
 LogFormat = '[{time:D-MM-YYYY H:mm:ss}] {runtime}:{line} {level} - {message}'
